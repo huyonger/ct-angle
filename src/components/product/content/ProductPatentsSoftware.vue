ff76eb88c6b3da59c1d598eca149cae4c81f0ada
huyonger <2385410383@qq.com>
2021-05-03 16:22:56 +0800

    --story=1008615 --user=路上飞 联系我们 https://www.tapd.cn/34237795/s/1013755

---
+++ src/components/product/content/ProductPatentsSoftware.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="patent-software">
+    <h2>
+      <span>专利与软著</span>
+      <span>PATENT SOFTWARE</span>
+    </h2>
+    <div class="content">
+      <div class="img"><img src="../../../assets/images/patent.png" alt=""></div>
+      <p class="title"><a href="../../../../static/software.doc" download="../../../../static/software.doc">点击下载</a></p>
+      <br><br><br><br><br><br><br><br><br><br>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProductPatentsSoftware"
+  }
+</script>
+
+<style scoped>
+  #patent-software {
+    padding: 20px;
+    background-color: #ffffff;
+  }
+
+  h2 {
+    display: inline-block;
+    border-bottom: 5px solid #324e7b;
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+
+  h2 span {
+    height: 40px;
+    line-height: 40px;
+  }
+
+  h2 span:nth-child(1) {
+    font-size: 18px;
+    color: #333333;
+  }
+
+  h2 span:nth-child(2) {
+    font-size: 16px;
+    color: #9c9c9c;
+  }
+
+  .content {
+    padding: 0 15px;
+    text-align: center;
+  }
+
+  .img{
+    text-align: center;
+  }
+
+
+  .content p {
+    width: 100%;
+    font-size: 14px;
+    text-indent: 34px;
+    line-height: 26px;
+  }
+
+  .content .title {
+    position: relative;
+    font-size: 16px;
+    color: #324e7b;
+    height: 30px;
+    line-height: 30px;
+    margin-bottom: 12px;
+    font-weight: 700;
+  }
+
+  a{
+    position: absolute;
+    right: 170px;
+  }
+
+</style>
