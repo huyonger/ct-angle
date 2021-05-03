09b4ca6c43cdf401c5d34edeedc3a17c581bd98a
huyonger <2385410383@qq.com>
2021-05-03 16:26:40 +0800

    --story=1008616 --user=路上飞 公司新闻 https://www.tapd.cn/34237795/s/1013756

---
+++ src/components/plain/Plain.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "Plain",
+    data(){
+      return{
+        path:''
+      }
+    },
+    created() {
+      if(this.$route.params.from==='ct-image'){
+        this.$router.push('/solve/ct-image')
+      }
+      if(this.$route.params.from==='medical-history'){
+        this.$router.push('/solve/medical-history')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
