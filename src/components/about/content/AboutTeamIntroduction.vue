3703ca3cedf56c80bb2502575d6259ee5588cc9e
huyonger <2385410383@qq.com>
2021-05-03 14:04:08 +0800

    --story=1008604 --user=yzr 就近就医 https://www.tapd.cn/34237795/s/1013647

---
+++ src/components/about/content/AboutTeamIntroduction.vue
@@ -0,0 +1,251 @@
+<template>
+  <div id="team-introduction">
+    <h2>
+      <span>团队介绍</span>
+      <span>TEAM INTRODUCTION</span>
+    </h2>
+    <div class="content">
+      <p class="title">湖北中地夺萃科技有限公司团队介绍</p>
+      <p>本团队由中国地质大学（武汉）近十位本科生组成，在地理与信息工程学院特聘副教授王永桂老师的指导下，秉持“负责、真诚、高效、精准、活力、卓越”的理念，是一支有能力、有朝气、肯吃苦、踏实奋进的年轻团队。</p>
+      <p class="title">团队负责人: 谢渝</p>
+      <div class="imgs">
+        <el-card shadow="hover">
+          <h3>谢渝</h3>
+          <div class="left">
+            <img src="../../../assets/images/man1.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>毕业设计方向为计量地理与GIS；本科期间多次获得校级奖学金；英语能力突出，雅思6.5分；在2019年9月曾受英国杜伦大学邀请，赴英学习；在2018年暑假参与湖北省地理综合实习。</p>
+            <p>作为团队负责人和企业法人，语言表达能力较强，英语口语出色，同时思维活跃，往往能提出别具一格的想法。</p>
+          </div>
+        </el-card>
+      </div>
+      <p class="title">开发小组: 胡勇、张德昊、李佳丽、韩雪洁、高尚、张晨豪、张迪雅</p>
+      <p class="title">开发小组负责人: 胡勇</p>
+      <p class="title">开发小组副负责人: 张德昊、李佳丽</p>
+      <div class="imgs">
+        <el-card shadow="hover">
+          <h3>胡勇</h3>
+          <div class="left">
+            <img src="../../../assets/images/man2.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>了解数据结构和算法，了解计算机基础。</p>
+            <p>掌握 maven、tomcat等软件、了解Linux操作。</p>
+            <p>掌握HTML、CSS、JavaScript 、Bootstrap等技术。</p>
+            <p>了解Vue和Nodejs等技术，能够简单运用Vue、ElementUI 和Axios进行开发。</p>
+            <p>了解Antv和Map Lab等数据可视化技术，能够运用ArcMap软件进行基本的数据分析。</p>
+            <p>热爱前端开发，对前端技术研究有强烈兴趣，具备良好的学习能力和分析问题解决能力责任心强，有良好的对外沟通和团队协作能力。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>李佳丽</h3>
+          <div class="left">
+            <img src="../../../assets/images/woman1.png" alt="">
+          </div>
+          <div class="right">
+            <p>中国地质大学（武汉）地理与信息工程学院本科生。</p>
+            <p>具有“互联网+”参赛经历，多次参与软件设计及开发，具有项目开发经验。</p>
+            <p>目前掌握html、JavaScript等基本技术，正在学习uni-app中，对软件开发感兴趣，具有良好的学习能力和团队意识。</p>
+            <p>在未来阶段，将主要负责项目“远程医疗诊断”手机软件的开发。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>张德昊</h3>
+          <div class="left">
+            <img src="../../../assets/images/zhangdehao.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>中国地质大学（武汉）地理与信息工程学院本科生。</p>
+            <p>具有良好的算法以及数据结构基础，了解计算机基础，熟悉linux的命令。</p>
+            <p>熟悉MFC以及pyqt界面开发。</p>
+            <p>熟悉基于keras的深度学习开发，同时了解计算机视觉中基于open3d库的开发。</p>
+            <p>擅长计算机后端，对于算法具有强烈的兴趣，同时擅长独立学习和思考，具有良好的自我认知，积极反思，具有较强的语言表达能力。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>韩雪洁</h3>
+          <div class="left">
+            <img src="../../../assets/images/hanxuejie.png" alt="">
+          </div>
+          <div class="right">
+            <p>了解数据结构和算法，了解计算机基础。</p>
+            <p>掌握HTML、CSS、JavaScript的基础使用。</p>
+            <p>掌握交互原型设计软件Axure的使用。</p>
+            <p>掌握数据库知识，熟悉MySQL数据库。</p>
+            <p>了解安卓开发，能够简单运用Android Studio进行开发。</p>
+            <p>了解Linux操作命令，了解Java基础知识。</p>
+            <p>性格热情开朗，为人真诚，能吃苦耐劳，脚踏实地。学习态度认真，具备良好的学习能力，有团结合作精神。。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>高尚</h3>
+          <div class="left">
+            <img src="../../../assets/images/gaoshang.png" alt="">
+          </div>
+          <div class="right">
+            <p>了解数据结构与算法，了解计算机组成原理与操作系统。</p>
+            <p>掌握c++，python，matlab等语言，了解Linux操作基础。</p>
+            <p>了解机器学习及机器学习的基础知识，掌握使用pytorch搭建神经网络的操作。</p>
+            <p>熟悉基于keras的深度学习开发，同时了解计算机视觉中基于open3d库的开发。</p>
+            <p>擅长数学模型的搭建，并在多项比赛中获奖。热衷于后端开发 。有良好的学习能力，分析问题解决问题能力强，有良好的团队合作能力。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>张晨豪</h3>
+          <div class="left">
+            <img src="../../../assets/images/zhangchenghao.png" alt="">
+          </div>
+          <div class="right">
+            <p>了解数据结构和算法，了解计算机基础。</p>
+            <p>掌握matlab，QT等软件，了解linux操作。</p>
+            <p>具备良好的学习能力和分析问题解决能力。</p>
+            <p>责任心强，有良好的沟通和团队协作能力。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>张迪雅</h3>
+          <div class="left">
+            <img src="../../../assets/images/zhangdiya.png" alt="">
+          </div>
+          <div class="right">
+            <p>2019年9月至今就读于中国地质大学（武汉）软件工程专业。</p>
+            <p>熟练掌握编程语言C/C++、Python、SQL及图形界面编程QT。</p>
+            <p>了解Tensorflow机器学习框架。</p>
+            <p>获信息调研大赛三等奖、科技论文报告会二等奖、湖北省翻译大赛三等奖、实用新型专利一项，担任大学生创新创业训练项目负责人，具备良好的科研思维、科学素养以及计算机基础。</p>
+          </div>
+        </el-card>
+      </div>
+      <p class="title">商业小组: 王子鑫、魏振坤、刘光杰</p>
+      <p class="title">商业小组HR: 王子鑫</p>
+      <div class="imgs">
+        <el-card shadow="hover">
+          <h3>王子鑫</h3>
+          <div class="left">
+            <img src="../../../assets/images/wangzixin.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>王子鑫，女，中共党员，经济管理学院2018级本科，多次参加创新创业比赛，负责撰写商业计划书与路演答辩等，发挥了经管学子的能力。多次代表学校参加如互联网加、地质加及各省市的创新创业大赛，并获得国家级、省级荣誉。目前对于创新创业有自己一定的见解。</p>
+          </div>
+        </el-card>
+        <el-card shadow="hover">
+          <h3>魏振坤</h3>
+          <div class="left">
+            <img src="../../../assets/images/weizhengkun.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>魏振坤，工商管理（双语）专业学生，热爱商科，热爱生活。曾获“院士奖学金"，曾多次参与大创、互联网+、挑战杯等比赛。学习成绩优良，综合素质突出，英语水平较高。思维活跃，能够提出新颖的想法。同时为人谨慎细致，是团队管理决策者的好参谋！</p>
+          </div>
+        </el-card>
+
+        <el-card shadow="hover">
+          <h3>刘光杰</h3>
+          <div class="left">
+            <img src="../../../assets/images/liuguangjie.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>大一时曾担任团支书，并被评为“优秀共青团干部”，持续参加众多社会实践活动和赛事，并获得一定荣誉，如“把地大带回家”社会实践团队一等奖，旅游风采大赛二等奖；此外，还在大学生创新创业项目、互联网+等大型赛事中撰写商业计划书、PPT等，比赛经验较丰富。</p>
+            <p>生活中积极开朗，对外沟通能力强，能结合相关资料提出新奇的想法。</p>
+          </div>
+        </el-card>
+      </div>
+      <p class="title">公众号运营: 吴和熹</p>
+      <p class="title">公众号运营负责人: 吴和熹</p>
+      <div class="imgs">
+        <el-card shadow="hover">
+          <h3>吴和熹</h3>
+          <div class="left">
+            <img src="../../../assets/images/wuhexi.jpg" alt="">
+          </div>
+          <div class="right">
+            <p>我是吴和熹，广东中山人，目前就读于中国地质大学（武汉）计算机学院。我任职校学生会组织联络部部员，曾运营CUGSU微信公众号平台，审核编辑推送内容。在过去一年半里成绩优异，保持较高的GPA，并获得过许多奖项。我期待着能在中地夺萃团队运营部门发挥我的专业和特长，与团队共同成长！</p>
+          </div>
+        </el-card>
+        <br><br><br><br><br><br><br><br><br><br>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AboutTeamIntroduction"
+  }
+</script>
+
+<style scoped>
+  #team-introduction {
+    padding: 20px;
+    background-color: #ffffff;
+  }
+
+  h2 {
+    display: inline-block;
+    border-bottom: 5px solid #324e7b;
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+
+  h2 span {
+    height: 40px;
+    line-height: 40px;
+  }
+
+  h2 span:nth-child(1) {
+    font-size: 18px;
+    color: #333333;
+  }
+
+  h2 span:nth-child(2) {
+    font-size: 16px;
+    color: #9c9c9c;
+  }
+
+  .content {
+    padding: 0 15px;
+  }
+
+
+  .content p {
+    font-size: 14px;
+    text-indent: 34px;
+    line-height: 26px;
+  }
+
+  .content .title {
+    font-size: 16px;
+    color: #324e7b;
+    height: 30px;
+    line-height: 30px;
+    margin-bottom: 12px;
+    font-weight: 700;
+  }
+
+  .imgs{
+    padding-left: 28px;
+  }
+
+  .left{
+    float: left;
+    width: 150px;
+    height: 200px;
+    background-color: pink;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .left img{
+    width: 100%;
+    height: 100%;
+  }
+
+  .right{
+    float: left;
+    width: 550px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-left: 30px;
+  }
+
+
+</style>
